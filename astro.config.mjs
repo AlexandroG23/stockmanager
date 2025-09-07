143b08d74ec694d11d8e249633da3a5db15c4ed9
alexandro <u18311432@utp.edu.pe>
2025-09-07 17:05:36 -0500

    add github pages deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://alexandrog23.github.io",
   base: '/stockmanager',
   integrations: [icon()],
 
