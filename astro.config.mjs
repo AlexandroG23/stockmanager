08251b4d118ae103e9f522114c6e8d55552cc50a
alexandro <u18311432@utp.edu.pe>
2025-09-07 16:52:02 -0500

    delete deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://alexandrog23.github.io',
   base: '/stockmanager',
   integrations: [icon()],
 
